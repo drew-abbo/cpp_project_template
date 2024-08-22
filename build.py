b5b678137f4fea9b67fbd362da5e8dad670de667
drew-abbo <drew.a.abbo@gmail.com>
2024-08-21 20:31:42 -0400

    build.py better supports CMake's 'GLOB_RECURSE'

--- build.py
+++ build.py
@@ -10,6 +10,8 @@ BUILD_DIR = Path("build")
 
 CALL_NAME = "build.py" if len(sys.argv) < 1 else sys.argv[0]
 
+__ON_WINDOWS = os.name == "nt"
+
 
 class Print:
     @staticmethod
@@ -101,7 +103,7 @@ def __get_cmds(build_in_parallel: bool, release_mode: bool) -> tuple[str, str]:
     config_cmd = "cmake .."
     build_cmd = "cmake --build ."
 
-    if os.name == "nt":
+    if __ON_WINDOWS:
         # Assume we're using Visual Studio (MSVC) on windows
         config_cmd += ' -G "Visual Studio 17 2022"'
     else:
@@ -109,7 +111,7 @@ def __get_cmds(build_in_parallel: bool, release_mode: bool) -> tuple[str, str]:
         config_cmd += ' -G "Unix Makefiles"'
 
     if release_mode:
-        if os.name == "nt":
+        if __ON_WINDOWS:
             # Release mode needs to be specified on build for MSVC
             build_cmd += " --config Release"
         else:
@@ -134,7 +136,7 @@ def run_cmd(
     print_cmd_output: bool = True,
 ) -> int:
     in_dir = str(in_dir) if in_dir is not None else None
-    cmd = cmd_str if os.name == "nt" else [cmd_str]
+    cmd = cmd_str if __ON_WINDOWS else [cmd_str]
     returncode = subprocess.run(
         cmd,
         check=False,
@@ -156,13 +158,20 @@ def __run_configure_cmd(config_cmd: str) -> None:
     Print.status(f"Configuring done ({time.time() - START_TIME:.3f} seconds).")
 
 
+def __run_regen_build_files_cmd() -> None:
+    START_TIME = time.time()
+    Print.status("Regenerating build files...")
+    run_cmd("cmake .", BUILD_DIR)
+    Print.status(f"Regenerating build files done ({time.time() - START_TIME:.3f} seconds).")
+
+
 def __run_build_cmd(build_cmd: str, release_mode: bool) -> None:
     START_TIME = time.time()
     Print.status("Building...")
     run_cmd(build_cmd, BUILD_DIR)
     Print.status(f"Building done ({time.time() - START_TIME:.3f} seconds).")
 
-    if os.name == "nt":
+    if __ON_WINDOWS:
         built_exe_folder = BUILD_DIR / ("Release" if release_mode else "Debug")
     else:
         built_exe_folder = BUILD_DIR
@@ -204,6 +213,8 @@ def build(build_in_parallel: bool, release_mode: bool) -> Path:
             else:
                 # just run the build cmd and exit if the commands are the same
                 if BUILD_JSON_CONTENTS == existing_json_contents:
+                    # regen build files to account for CMake GLOB_RECURSE
+                    __run_regen_build_files_cmd()
                     __run_build_cmd(build_cmd, release_mode)
                     return
                 else:
